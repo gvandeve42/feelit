525d459abb383464b060e080fcf0b038ac572154
Guillaume VANDEVELDE <gvandeve@e1r4p6.42.fr>
2016-11-30 20:12:54 +0100

    Amelio esthetique

--- feelit.sh
+++ feelit.sh
@@ -6,7 +6,7 @@
 #    By: gvandeve <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2016/11/23 17:33:03 by gvandeve          #+#    #+#              #
-#    Updated: 2016/11/30 19:44:00 by gvandeve         ###   ########.fr        #
+#    Updated: 2016/11/30 20:12:00 by gvandeve         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -35,7 +35,7 @@ do
 	$PATHF/fillit test_i/$file > ./logs/user/invalid/user_$file.txt && \
 		echo "\033[31;1m/!\\ MAUVAISE SORTIE /!\\\n[RESULTAT ENREGISTRE DANS ./logs/user/invalid/user_$file.txt]\033[0m" && \
 		cat ./logs/user/invalid/user_$file.txt || \
-		echo "\033[32;1m:\) BONNE SORTIE :\)\n\033[0m"
+		echo "\033[32;1m\o/ BONNE SORTIE \o/\n\033[0m"
 done
 
 echo
@@ -48,7 +48,7 @@ do
 	echo
 	echo "\033[44;1m---------- run on test_v/$file\033[0m"
 	$PATHF/fillit test_v/$file > ./logs/user/valid/user_$file.txt && \
-		echo "\033[32;1m:\) PAS D'ERREUR, EN ATTENTE DE LA DIFF :\)\n[RESULTAT ENREGISTRE DANS ./logs/user/valid/user_$file.txt]\033[0m" && \
+		echo "\033[32;1m\o/ PAS D'ERREUR, EN ATTENTE DE LA DIFF \o/\n[RESULTAT ENREGISTRE DANS ./logs/user/valid/user_$file.txt]\033[0m" && \
 		cat ./logs/user/valid/user_$file.txt || \
-		echo "\033[31;1m[ERREUR, FIN DU PROGRAMME]"
+		echo "\033[31;1m/!\\ [ERREUR, FIN DU PROGRAMME] /!\\"
 done
