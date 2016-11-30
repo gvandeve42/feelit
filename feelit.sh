0dcd418307ba2fb32c80955343d4442c645811e4
Guillaume VANDEVELDE <gvandeve@e1r4p6.42.fr>
2016-11-30 19:16:07 +0100

    Ameliorations et systeme de fichiers

--- feelit.sh
+++ feelit.sh
@@ -6,13 +6,16 @@
 #    By: gvandeve <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2016/11/23 17:33:03 by gvandeve          #+#    #+#              #
-#    Updated: 2016/11/29 14:54:19 by gvandeve         ###   ########.fr        #
+#    Updated: 2016/11/30 18:47:53 by gvandeve         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
 PATHF=~/work/fillit
+PATHR=./logs/results/
+PATHU=./logs/user/
+PATHD=./logs/
 
 clear
 
@@ -31,16 +34,11 @@ echo "--------------------------------------------------\033[0m"
 for file in $(ls test_i/ | sort)
 do
 	echo
-	echo "\033[44;1m---------- run on test_i/$file"
-	$PATHF/fillit test_i/$file
-	result=$(echo $?)
-	echo $(./f_strerror $result)
-	if [ $result == 1 ]
-	then
-		echo "\033[32;1mOK\033[0m"
-	else
-		echo "\033[31;1mKO\033[0m"
-	fi
+	echo "\033[44;1m---------- run on test_i/$file\033[0m"
+	$PATHF/fillit test_i/$file > ./logs/user/invalid/user_$file.txt && \
+		echo "\033[31;1m/!\\ MAUVAISE SORTIE /!\\\n[RESULTAT ENREGISTRE DANS ./logs/user/invalid/user_$file.txt]\033[0m" && \
+		cat ./logs/user/invalid/user_$file.txt || \
+		echo "\033[32;1m:\) BONNE SORTIE :\)\n\033[0m"
 done
 
 echo
@@ -51,14 +49,9 @@ echo "--------------------------------------------------\033[0m"
 for file in $(ls test_v/ | sort)
 do
 	echo
-	echo "\033[44;1m---------- run on test_i/$file"
-	$PATHF/fillit test_v/$file
-	result=$(echo $?)
-	echo $(./f_strerror $result)
-	if [ $result == 0 ]
-	then
-		echo "\033[32;1mOK\033[0m"
-	else
-		echo "\033[31;1mKO\033[0m"
-	fi
+	echo "\033[44;1m---------- run on test_i/$file\033[0m"
+	$PATHF/fillit test_v/$file > ./logs/user/valid/user_$file.txt && \
+		echo "\033[32;1m:\) PAS D'ERREUR, EN ATTENTE DE LA DIFF :\)\n[RESULTAT ENREGISTRE DANS ./logs/user/valid/user_$file.txt]\033[0m" && \
+		cat ./logs/user/valid/user_$file.txt || \
+		echo "\033[31;1m[ERREUR, FIN DU PROGRAMME]"
 done
